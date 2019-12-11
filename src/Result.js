7c1e44440a0feecce717220637aba262ef07a7bc
michael9020 <michael.lee.plunkett@gmail.com>
2019-12-11 16:20:47 +0900

    Better formatting

--- src/Result.js
+++ src/Result.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const Result = props => {
-  return (
-    <div className="results box">
-      <p>Original Title: <b><em>{props.loading ? 'searching...' : props.movieDBOriginalResult}</em></b></p>
-      <p>Japanese Title: <b><em>{props.loading ? 'searching...' : props.movieDBJPResult}</em></b></p>
-      <br/>
-      <p>Data provided by&nbsp;
-        <a href="https://www.themoviedb.org/">The Movie Database</a>
-      </p>
-    </div>
-  )
-}
+const Result = props => (
+  <div className="results box">
+    <p>Original Title: <b><em>{props.loading ? 'searching...' : props.movieDBOriginalResult}</em></b></p>
+    <p>Japanese Title: <b><em>{props.loading ? 'searching...' : props.movieDBJPResult}</em></b></p>
+    <br/>
+    <p>Data provided by&nbsp;
+      <a href="https://www.themoviedb.org/">The Movie Database</a>
+    </p>
+  </div>
+)
 
 export default Result
