e3b283d80ca6d3d1e50358899c2bbcfd28ef46c5
Plunkett, Michael <MPlunkett@sallybeauty.com>
2023-11-10 15:36:39 -0600

    Links & style

--- src/Result.js
+++ src/Result.js
@@ -6,10 +6,6 @@ const Result = props => {
   <div className="results box">
     <h4>Original: <span className="return-title">{props.loading ? 'searching...' : props.movieDBOriginalResult}</span></h4>
     <h4>{props.language.name} ({props.language.engName}): <span className="return-title">{props.loading ? 'searching...' : props.movieDBTRResult}</span></h4>
-    <br/>
-    <p>Data provided by&nbsp;
-      <a href="https://www.themoviedb.org/">The Movie Database</a>
-    </p>
   </div>
 )}
 
